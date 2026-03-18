082d77d58d55d49b2838501dc987460eb14e2354
KusaTea <matthewkhoimov@yandex.ru>
2026-03-18 16:04:59 +0200

    participant model was edited

--- model/participant_data/participant_data.py
+++ model/participant_data/participant_data.py
@@ -2,6 +2,7 @@ import h5py
 from datetime import datetime
 from typing import Literal
 from pathlib import Path
+import os
 
 
 class ParticipantData:
@@ -18,9 +19,13 @@ class ParticipantData:
         self.hand = hand
 
     def save_participant_info(self, save_dir: Path):
-        with h5py.File(save_dir / (str(self.code) + '.hdf5'), 'w') as save_file:
-            save_file.attrs['participant_code'] = self.code
-            save_file.attrs['participant_age'] = self.code
-            save_file.attrs['participant_gender'] = self.gender
-            save_file.attrs['participant_leading_hand'] = self.hand
-            save_file.attrs['date'] = str(datetime.now())
+        if not os.path.exists(save_dir / (str(self.code) + '.hdf5')):
+            with h5py.File(save_dir / (str(self.code) + '.hdf5'), 'w') as save_file:
+                save_file.attrs['participant_code'] = self.code
+                save_file.attrs['participant_age'] = self.code
+                save_file.attrs['participant_gender'] = self.gender
+                save_file.attrs['participant_leading_hand'] = self.hand
+                save_file.attrs['date'] = str(datetime.now())
+        
+        else:
+            raise FileExistsError('File for such code already exists')
