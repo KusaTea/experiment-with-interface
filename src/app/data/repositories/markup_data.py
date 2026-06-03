f7993a06a9cd7797be9a415885737f8dadf7af6e
KusaTea <matthewkhoimov@gmail.com>
2026-06-03 12:46:44 +0200

    fix: data module was uploaded

---
+++ src/app/data/repositories/markup_data.py
@@ -0,0 +1,52 @@
+import json
+from pathlib import Path
+
+import numpy as np
+
+from data.models import MarkupDataType
+
+
+class MarkupDataCreater:
+
+    def __init__(self, file_dir: Path):
+        self.save_file = open(file_dir / 'markup.json', 'w')
+    
+    def close_file(self):
+        self.save_file.close()
+
+    def __del__(self):
+        try:
+            self.save_file.close()
+        except:
+            pass
+
+    def save_data(self, timestamp: str, exercise: int):
+        json.dump({timestamp: exercise}, self.save_file)
+        self.save_file.write('\n')
+
+
+class MarkupDataReader:
+
+    def __init__(self, file_dir: Path):
+
+        self.file_dir = file_dir / 'markup.json'
+
+    def load_data(self) -> MarkupDataType:
+        '''Returns two arrays: timestamps and exercises indeces'''
+        data = list()
+
+        with open(self.file_dir, 'r') as f:
+            while line := f.readline():
+                try:
+                    line_data = json.loads(line.strip())
+                except:
+                    continue
+
+                data.append(list(line_data.items())[0])
+        
+        data = np.array(data)
+
+        return {
+            'timestamps': data[:, 0].astype(np.float32),
+            'exercises': data[:, 1].astype(np.int32)
+            }
---
+++ src/app/data/repositories/quattrocento_data_handler.py
@@ -0,0 +1,100 @@
+import multiprocessing as mlp
+from multiprocessing.synchronize import Event
+from pathlib import Path
+import h5py
+from time import time
+
+from data.abstracts import ModuleAbstract, DataHandlerAbstract
+from data.repositories import QuattrocentoSettings
+
+
+class QuattrocentoDataHandler(DataHandlerAbstract, mlp.Process):
+
+    def __init__(
+            self,
+            module: ModuleAbstract,
+            save_folder_dir: Path,
+            start_flag: Event,
+            abort_flag: Event,
+            quattrocento_settings: QuattrocentoSettings
+        ):
+        mlp.Process.__init__(self)
+        self.__module = module
+        self.__save_file_dir = save_folder_dir / 'emg.hdf5'
+        self.__start_flag = start_flag
+        self.__abort_flag = abort_flag
+        self.__settings = quattrocento_settings
+
+        self.__is_ready = False
+
+
+    @property
+    def is_ready(self) -> bool:
+        return self.__is_ready
+
+    
+    def __check_module_connection(self):
+        try:
+            self.__module.send_data(self.__settings.get_bytes())
+            self.__is_ready =  self.__module.check_connection()
+
+        except BaseException:
+            self.__is_ready = False
+        
+        finally:
+            self.__module.send_data(self.__settings.get_stop_command_bytes())
+
+
+
+    def start_record(self):
+        with h5py.File(self.__save_file_dir, mode='w') as save_file:
+            save_file.attrs['sampling_rate'] = self.__settings.sampling_rate
+            save_file.attrs['num_of_channels'] = self.__settings.num_of_channels
+            save_file.attrs["mV_constant"] = self.__settings.mV_constant
+            save_file.attrs['readme'] = 'data should be reshaped (sampling_rate, num_of_channels); each value must bu multiplied by mV_constant'
+
+            self.__module.send_data(self.__settings.get_bytes())
+            
+            while not self.__abort_flag.is_set():
+                try:
+                    data = self.__module.receive_data(2)
+                    ts = str(time())
+
+                    save_file.create_dataset(
+                        name=ts,
+                        data=data
+                        )
+                except BaseException as e:
+                    self.__abort_flag.set()
+                    raise e
+
+
+    def stop_record(self):
+        self.__module.send_data(self.__settings.get_stop_command_bytes())
+        self.__module.close_connection()
+        
+        if self.__module.is_connected:
+            raise ConnectionError('Couldn\'t stop the connection')
+
+        self.__is_ready = False
+
+
+    def connect_module(self) -> bool:
+        try:
+            self.__module.connect()
+            self.__check_module_connection()
+            return self.__is_ready
+        except BaseException as e:
+            self.__abort_flag.set()
+            raise e
+
+
+    def run(self):
+        if not self.__is_ready:
+            self.connect_module()
+
+        while not self.__start_flag.is_set():
+            continue
+
+        self.start_record()
+        self.stop_record()
